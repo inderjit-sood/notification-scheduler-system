59a7dc7673d21da39d75d75e9c4efa166820938b
Inderjit <sirs025@gmail.com>
2024-11-02 22:25:10 +0530

    Added /update/ and /delete/ endpoints to the NotificationController. Added ConcurrentHashMap for inmemory database

--- src/main/java/com/goodapps/application/controllers/NotificationController.java
+++ src/main/java/com/goodapps/application/controllers/NotificationController.java
@@ -4,14 +4,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 @RequestMapping("api/notification")
 public class NotificationController {
+    private Map<Long, String> notificationMap = new ConcurrentHashMap<>();
+    private AtomicLong idAssign = new AtomicLong(0);
     @PostMapping("/create")
     public ResponseEntity<String> createNotification(@RequestBody String notification) {
         try {
-            System.out.println("Got notification creation requeest: " + notification);
-            return new ResponseEntity<>(notification+"1", HttpStatus.CREATED);
+            System.out.println("Got notification creation request: " + notification);
+            Long idToAssign = idAssign.incrementAndGet();
+            notificationMap.put(idToAssign, notification);
+            return new ResponseEntity<>(notification, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -20,12 +28,39 @@ public class NotificationController {
     @GetMapping("/get/{id}")
     public ResponseEntity<String> getNotification(@PathVariable long id) {
         try {
-            if(id == 1) {
-                System.out.println("Got a valid GET request");
-                return new ResponseEntity<>("1Notification", HttpStatus.OK);
-            } else throw new Exception("bad");
+            if(!notificationMap.containsKey(id)) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            String notification = notificationMap.get(id);
+            return new ResponseEntity<>(notification, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateNotification(@PathVariable long id, @RequestBody String notification){
+        try {
+            if(!notificationMap.containsKey(id)) {
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+            System.out.println("Updating id "+id+ " notification to the new notification: "+notification);
+            notificationMap.put(id, notification);
+            return new ResponseEntity<>(notification, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>("update cancelled", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteNotification(@PathVariable long id) {
+        try {
+            if(!notificationMap.containsKey(id)) {
+                return new ResponseEntity<>("Invalid Id", HttpStatus.BAD_REQUEST);
+            }
+            System.out.println("Deleting notification with id = "+id);
+            notificationMap.remove(id);
+            return new ResponseEntity<>("Notification deleted successfully", HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>("There was some server error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
