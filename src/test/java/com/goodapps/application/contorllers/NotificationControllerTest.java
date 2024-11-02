b8acf2c776d7e5c63510ff2e510e7a8a145695e1
Inderjit <sirs025@gmail.com>
2024-11-03 00:57:44 +0530

    JUNIT 5 setup (BOM, dependency, build plugin) and added test case for Notification Controller

---
+++ src/test/java/com/goodapps/application/contorllers/NotificationControllerTest.java
@@ -0,0 +1,17 @@
+package com.goodapps.application.contorllers;
+import com.goodapps.application.controllers.NotificationController;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NotificationControllerTest {
+    NotificationController notificationController = new NotificationController();
+    @Test
+    void test_createNotification() {
+        String notificationRequest = "New Notification";
+        ResponseEntity<String> response = notificationController.createNotification(notificationRequest);
+        assertEquals(notificationRequest, response.getBody());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+}
